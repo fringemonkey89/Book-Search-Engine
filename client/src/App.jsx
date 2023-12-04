95bbbc14ad90611d9c7c0e77f3b42db8861305b8
fringemonkey89 <markomajeticjr@gmail.com>
2023-12-04 11:08:16 -0500

    added dependencies and code to client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import './App.css';
+import { ApolloProvider, ApolloClient, inMemoryCache, createHttpLink} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import { Outlet } from 'react-router-dom';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import SearchBooks from './pages/SearchBooks'
+import SavedBooks from './pages/SavedBooks'
 import Navbar from './components/Navbar';
+
+const httpLink = createHttpLink({
+  uri: '/graphql'
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new inMemoryCache()
+})
 
 function App() {
   return (
